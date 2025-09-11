75921cde3b330088631fd4243afb6ab78199cfa5
Simon <simon.zhao@milesight.com>
2025-09-11 11:41:11 +0800

    fix flatten script

--- devices/milesight/common/codec/flatten.js
+++ devices/milesight/common/codec/flatten.js
@@ -3,28 +3,27 @@ function process(obj) {
   const result = {};
   const shouldBeFlatten = (o) => typeof o === 'object'
     && o !== null
+    && Object.keys(o).length > 0
     && !Array.isArray(o);
 
-  function flattenSub(subObj, prefix, res) {
+  function flattenSub(subObj, prefix) {
     if (prefix && Object.keys(subObj).length === 0 && !Array.isArray(subObj)) {
-      res[prefix] = {};
       return;
     }
 
     for (const k in subObj) {
-      const newPrefix = prefix ? prefix + '#' + k : k;
+      const newPrefix = prefix ? prefix + (prefix.includes('.') ? '#' : '.') + k : k;
       if (shouldBeFlatten(subObj[k])) {
-        flattenSub(subObj[k], newPrefix, res);
+        flattenSub(subObj[k], newPrefix);
       } else {
-        res[newPrefix] = subObj[k];
+        result[newPrefix] = subObj[k];
       }
     }
   }
 
   for (const key in obj) {
     if (shouldBeFlatten(obj[key])) {
-      result[key] = {};
-      flattenSub(obj[key], '', result[key]);
+      flattenSub(obj[key], key);
     } else {
       result[key] = obj[key];
     }
--- devices/milesight/common/codec/unflatten.js
+++ devices/milesight/common/codec/unflatten.js
@@ -2,26 +2,20 @@ function process(obj) {
   const result = {};
 
   for (const key in obj) {
-    const subObj = obj[key];
-    if (typeof subObj === 'object' && subObj !== null && !Array.isArray(subObj)) {
-      const restored = {};
-      for (const flatKey in subObj) {
-        const value = subObj[flatKey];
-        const parts = flatKey.split('#');
-        let cur = restored;
-        for (let i = 0; i < parts.length; i++) {
-          const part = parts[i];
-          if (i === parts.length - 1) {
-            cur[part] = value;
-          } else {
-            if (!(part in cur)) cur[part] = {};
-            cur = cur[part];
-          }
+    const path = key.split(/\.|#/g);
+    let curr = result;
+
+    for (let i = 0; i < path.length; i++) {
+      const part = path[i];
+
+      if (i === path.length - 1) {
+        curr[part] = obj[key];
+      } else {
+        if (!(part in curr)) {
+          curr[part] = {};
         }
+        curr = curr[part];
       }
-      result[key] = restored;
-    } else {
-      result[key] = obj[key];
     }
   }
 
